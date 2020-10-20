7c341cfd1accb9f7c58284693f4a90a8187274c1
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-10-20 03:09:46 -0600

    Varios

--- cifrado.cpp
+++ cifrado.cpp
@@ -25,16 +25,27 @@
 using namespace std;
 
 //Variables globales --------------------------------------------------------------------------------------------------------------------------------------------------
-int* PLAIN;
+char* PLAIN;
 char KEY[16];
 int SIZE = 16;
 const int ROUNDS = 4;
 char ALLKEYS[16*ROUNDS];
 int MODULE;
 
+pthread_mutex_t lock;
 
-
-void rotateKeyCipher();
+void escribir(char chars[], int init){
+	ofstream archivo1;
+	archivo1.open("cipher.bin",ios_base::app);//Abriendo archivo en modo escritura
+	if(archivo1.fail()){
+		cout<<"No se pudo abrir el archivo";
+		exit(1);
+	}
+	for(int i=init; i<(SIZE+init); i++){
+		archivo1<<chars[i];
+	}
+	archivo1.close();
+}
 
 //Funcion para pthreads -----------------------------------------------------------------------------------------------------------------------------------------------
 void *cifrar(void *thread_rank){
@@ -43,16 +54,23 @@ void *cifrar(void *thread_rank){
 	tID = (int)thread_rank;
     int cont;
 
+    char temp[SIZE*4];
+
     //Encriptar bloque asignado
     for(int i=0; i<ROUNDS; i++){
         cont = 0;
-        for(int j=tID; j<=(tID+SIZE); j++){
-            PLAIN[j] = char(int(PLAIN[j])+int(ALLKEYS[cont+(i*16)]));
+        for(int j=tID; j<(tID+SIZE); j++){
+            temp[j] = char((int)PLAIN[j]+(int)ALLKEYS[cont+(i*16)]);
+            cout<<PLAIN[j];
             cont++;
         }
-    }
+    }cout<<endl;
 
-    
+    //Escribir bloque en el documento
+    pthread_mutex_lock(&lock);
+    escribir(temp, tID);
+    pthread_mutex_unlock(&lock);
+
     //Terminar el proceso
     pthread_exit(NULL);
 }
@@ -63,14 +81,21 @@ void *descifrar(void *thread_rank){
 	tID = (int)thread_rank;
     int cont;
 
+    char temp[SIZE*4];
+
     //Encriptar bloque asignado
     for(int i=0; i<ROUNDS; i++){
         cont = 0;
-        for(int j=tID; j<=(tID+SIZE); j++){
-            PLAIN[j] = char(int(PLAIN[j])-int(ALLKEYS[cont+(i*16)]));
+        for(int j=tID; j<(tID+SIZE); j++){
+            temp[j] = char(int(PLAIN[j])+int(ALLKEYS[cont-(i*16)]));
             cont++;
         }
     }
+
+    //Escribir bloque en el documento
+    pthread_mutex_lock(&lock);
+    escribir(temp, tID);
+    pthread_mutex_unlock(&lock);
 
     //Terminar el proceso
     pthread_exit(NULL);
@@ -91,19 +116,6 @@ void rotateKeyCipher(){
     }
 }
 
-void escribir(char chars[]){
-	ofstream archivo1;
-	archivo1.open("llave.txt",ios_base::app);//Abriendo archivo en modo escritura
-	if(archivo1.fail()){
-		cout<<"No se pudo abrir el archivo";
-		exit(1);
-	}
-	for(int i=0; i<SIZE; i++){
-		archivo1<<chars[i];
-	}
-	archivo1.close();
-}
-
 void generateKey(){
     string my_key="";
     srand (time(NULL));
@@ -117,11 +129,17 @@ void generateKey(){
 
 void encriptar(){
     //Generar llave aleatoria
+    /*
     generateKey();
     cout<<"Llave: ";
     for(int i=0; i<SIZE; i++){
         cout<<KEY[i];
     }cout<<endl;
+    */
+    string my_key;
+    cout<<"Ingrese la llave (16): ";
+    cin>>my_key;
+    cout<<endl;
 
     for(int i=0; i<ROUNDS; i++){
         for(int j=0; j<SIZE; j++){
@@ -144,7 +162,70 @@ void encriptar(){
     //Determinar numero de caracteres
     lectura.seekg(0, ios_base::end);
     int nChars = lectura.tellg();
-    PLAIN = new int[nChars];
+    PLAIN = new char[nChars];
+    lectura.seekg(0, ios_base::beg);
+
+    //Almacenar mensaje del archivo en arreglo
+    char letra;
+    int i=0;
+    while(lectura.get(letra)){
+        PLAIN[i]=letra;
+        cout<<PLAIN[i];
+        i++;
+    }cout<<endl;
+
+    //Determinar cantidad de hilos a usar
+    int n = nChars/16;
+    MODULE = nChars%16;
+
+    //Distribuir tareas en pthreads
+    pthread_t threads[n+1];
+    int ptEstatus;
+    for(int i=0; i<=(n*SIZE); i+=SIZE){
+        //Crear hilos
+        ptEstatus = pthread_create(&threads[i], NULL, cifrar,(void *)i);
+        if (ptEstatus){
+            printf("ERROR; return code from pthread_create() is %d\n", ptEstatus);
+            exit(-1);
+        //Esperar a que finalicen
+        }ptEstatus = pthread_join(threads[i], NULL);
+        if (ptEstatus) {
+            printf("ERROR; return code from pthread_join() is %d\n", ptEstatus);
+            exit(-1);
+        }
+    }
+}
+
+void desencriptar(){
+    string my_key;
+    cout<<"Ingrese la llave: ";
+    cin>>my_key;
+    cout<<endl;
+
+    my_key.copy(KEY, SIZE + 1);
+
+    for(int i=0; i<ROUNDS; i++){
+        for(int j=0; j<SIZE; j++){
+            ALLKEYS[j+(i*16)] = KEY[j];
+        }
+        rotateKeyCipher();
+    }
+    cout<<"\n"<<endl;
+    for(int i=0; i<(SIZE*4); i++){
+        cout<<ALLKEYS[i];
+    }cout<<endl;
+
+    //Crear objeto de lectura para el archivo
+    ifstream lectura("cipher.bin",ios::in);
+    if(!lectura){
+        cerr<<"Fail to read cipher.bin"<<endl;
+        exit(EXIT_FAILURE);
+    }
+
+    //Determinar numero de caracteres
+    lectura.seekg(0, ios_base::end);
+    int nChars = lectura.tellg();
+    PLAIN = new char[nChars];
     lectura.seekg(0, ios_base::beg);
 
     //Almacenar mensaje del archivo en arreglo
@@ -160,24 +241,22 @@ void encriptar(){
     MODULE = nChars%16;
 
     //Distribuir tareas en pthreads
-    pthread_t threads;
+    pthread_t threads[n+1];
     int ptEstatus;
     for(int i=0; i<=(n*SIZE); i+=SIZE){
         //Crear hilos
-        ptEstatus = pthread_create(&threads, NULL, cifrar,(void *)i);
+        ptEstatus = pthread_create(&threads[i], NULL, descifrar,(void *)i);
         if (ptEstatus){
             printf("ERROR; return code from pthread_create() is %d\n", ptEstatus);
             exit(-1);
         //Esperar a que finalicen
-        }ptEstatus = pthread_join(threads, NULL);
+        }ptEstatus = pthread_join(threads[i], NULL);
         if (ptEstatus) {
             printf("ERROR; return code from pthread_join() is %d\n", ptEstatus);
             exit(-1);
         }
     }
 }
-
-void desencriptar(){}
 
 // Main ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 int main(){
